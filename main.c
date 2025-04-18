798d14d70d1a485c8956992002b504d57ef1107f
Hayden Freeman <haydenfreeman@yahoo.com>
2025-04-18 09:27:52 -0500

    AUTO COMMIT: 2025-04-18 09:27:52

--- main.c
+++ main.c
@@ -2,16 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* 
-uppercase(char* arg)
+char*
+safeCopy(char* arg)
 {
     char* str;
-    int i;
     if (arg == NULL) { return NULL; }
     str = malloc(strlen(arg) + 1);
     if (str == NULL) { return NULL; }
     strcpy(str, arg);
+    return str;
+}
 
+void
+printList(int size, char** lst)
+{
+    int i;
+    for (i = 0; i < size; i++) { printf("%d: %s\n", i, lst[i]); }
+}
+
+char* 
+uppercase(char* arg)
+{
+    int i;
+    char* str;
+
+    str = safeCopy(arg);
     i = 0;
     while (str[i] != '\0')
     {
@@ -21,17 +36,65 @@ uppercase(char* arg)
         }
         i++;
     }
-
     return str;
+}
+
+char**
+uppercaseList(int argc, char** argv)
+{
+    int i;
+    char** lst;
+    if (argc < 0) { exit(1); }
+    lst = malloc(sizeof(char*) * (size_t)argc);
+    for (i = 0; i < argc; i++) { lst[i] = uppercase(argv[i]); } 
+    return lst;
+}
+
+char*
+reverse(char* arg)
+{
+    size_t i,len;
+    char* str;
+    char temp;
+
+    str = safeCopy(arg);
+    len = strlen(str);
+    i = 0;
+    for (i = 0; i < len / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[len-i-1];
+        str[len-i-1] = temp;
+    }
+    return str;
+}
+
+char**
+reverseList(int argc, char** argv)
+{
+    int i;
+    char** lst;
+    if (argc < 0) { exit(1); }
+    lst = malloc(sizeof(char*) * (size_t)argc);
+    for (i = 0; i < argc; i++) { lst[i] = reverse(argv[i]); } 
+    return lst;
 }
 
 int
 main(int argc, char** argv)
 {
-    char* str;
-    printf("%d %s\n\n", argc, argv[0]);
+    char** lst1;
+    char** lst2;
+    int i;
 
-    str = uppercase(argv[0]);
-    printf("%s", str);
+    for (i = 0; i < argc; i++) { printf("argv[%d]: %s\n", i, argv[i] ? argv[i] : "NULL"); }
+
+    printf("uppercase: \n");
+    lst1 = uppercaseList(argc, argv);
+    printList(argc, lst1);
+
+    printf("reverse: \n");
+    lst2 = reverseList(argc, argv);
+    printList(argc, lst2);
     return 0;
 }
