fcec507e7d292a06dcf71017bf217945c65f7678
Hayden Freeman <haydenfreeman@yahoo.com>
2025-04-18 08:10:24 -0500

    AUTO COMMIT: 2025-04-18 08:10:24

--- main.c
+++ main.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* 
+uppercase(char* arg)
+{
+    char* str;
+    int i;
+    if (arg == NULL) { return NULL; }
+    str = malloc(strlen(arg) + 1);
+    if (str == NULL) { return NULL; }
+    strcpy(str, arg);
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 97 && str[i]<= 122)
+        {
+            str[i]-=32;
+        }
+        i++;
+    }
+
+    return str;
+}
 
 int
 main(int argc, char** argv)
 {
-    printf("count: %d\n", argc);
-    while (*argv != NULL)
-    {
-        printf("string: %s\n", *argv);
-        argv++;
-    }
+    char* str;
+    printf("%d %s\n\n", argc, argv[0]);
+
+    str = uppercase(argv[0]);
+    printf("%s", str);
     return 0;
 }
